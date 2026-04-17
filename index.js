4082ecee6970b63ab19aa3e3e119c80f198c4eca
Pranav <pranav@expertzlab.com>
2026-04-17 15:20:33 +0530

    new test api added

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send("Backend HOME page is RUNNING...")
 })
 
+app.get('/api/test', () => {
+    res.json({
+        message: "Hello message from backend...!",
+        status:"success"
+    })
+})
+
 app.listen(5000,'0.0.0.0', () => {
     console.log("Backend running on port : 5000")
 })
